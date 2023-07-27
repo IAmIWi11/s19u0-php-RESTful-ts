b62fbe582dc2651696407a3bce14be1bb7b69035
s19u0 <iamxzgsl@gmail.com>
2023-07-27 16:56:13 +0800

    test

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 's19u0-php-restful-ts/framework',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '706d97d7b79f29831e616154a40a93c393b3425b',
+        'reference' => 'e773d6d953ac517a60bab97d05226cc0d11f5a62',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         's19u0-php-restful-ts/framework' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '706d97d7b79f29831e616154a40a93c393b3425b',
+            'reference' => 'e773d6d953ac517a60bab97d05226cc0d11f5a62',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
